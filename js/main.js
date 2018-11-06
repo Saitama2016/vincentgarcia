d88a32af7849dbd14308a63e42f341042e4f43cc
Vincent Garcia <24626196+Saitama2016@users.noreply.github.com>
2018-11-05 19:15:53 -0800

    Icons and Nav Bar created

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
 $( () => {
     function activateSideNav () {
         $('#sideNavButton').click(function(e) {
-            console.log("click!")
+            $('#sideNav').css("width", "500px");
         });
     }
 
-    function handleEvent() {
-        activateSideNav();
+    function smoothScroll() {
+        $(document).on('click', 'a[href^="#"]', function (event) {
+            event.preventDefault();
+        
+            $('html, body').animate({
+              scrollTop: $($.attr(this, 'href')).offset().top
+             }, 500);
+
+            $('#sideNav').css("width", "0px");
+        });
     }
 
-    handleEvent();
+    function closeSideNav() {
+        $('.closebtn').click(function(e) {
+            $('#sideNav').css("width", "0px");
+        });
+    }
+
+    function handleEvents() {
+        activateSideNav();
+        smoothScroll();
+        closeSideNav();
+    }
+
+    handleEvents();
 });
