dc142f98520824a266da64539396e45129b7191d
Vincent Garcia <24626196+Saitama2016@users.noreply.github.com>
2018-11-20 19:04:19 -0800

    Follow NDA rules

--- js/main.js
+++ js/main.js
@@ -23,40 +23,11 @@ $( () => {
         });
     }
 
-    let slideIndex = 1;
-    showSlides(slideIndex);
-
-    // Next/previous controls
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
-
-    function slideButtons() {
-        $('.prev').click(function(e) {
-            plusSlides(-1);
-        })
-
-        $('.next').click(function(e) {
-            plusSlides(1);
-        })
-    }
-
-    function showSlides(n) {
-      let i;
-      let slides = $(".huluSlides");
-      if (n > slides.length) {slideIndex = 1} 
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none"; 
-      }
-      slides[slideIndex-1].style.display = "block"; 
-    }
 
     function handleEvents() {
         activateSideNav();
         smoothScroll();
         closeSideNav();
-        slideButtons();
     }
 
     handleEvents();
